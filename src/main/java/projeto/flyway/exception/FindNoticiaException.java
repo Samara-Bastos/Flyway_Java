f4aa0b904fb4b92668bf06ef566256717f142400
Samara Bastos <107880115+Samara-Bastos@users.noreply.github.com>
2024-07-15 16:21:58 -0300

    Retirando ids de versionamento

--- src/main/java/projeto/flyway/exception/FindNoticiaException.java
+++ src/main/java/projeto/flyway/exception/FindNoticiaException.java
@@ -1,8 +1,6 @@
 package projeto.flyway.exception;
 
 public class FindNoticiaException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
     public FindNoticiaException(String ex){
         super(ex);
     }
